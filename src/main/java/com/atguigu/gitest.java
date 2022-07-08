0c0f515d199d0aa8faeab9849b37a2d58d00890c
cosoice <108922419+cosoice@users.noreply.github.com>
2022-07-09 00:51:56 +0800

    gitee test

--- src/main/java/com/atguigu/gitest.java
+++ src/main/java/com/atguigu/gitest.java
@@ -14,5 +14,6 @@ public class gitest {
                 System.out.println("hot-fix commit");
                 System.out.println("push commit");
                 System.out.println("pull test");
+                System.out.println("gitee test");
         }
 }
