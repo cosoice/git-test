a05b46c19a645ed9fbc1e5720404c549bed32ce1
coso <coso@qq.com>
2022-07-08 23:44:41 +0800

    git-idea first commit

---
+++ src/main/java/com/atguigu/gitest.java
@@ -0,0 +1,11 @@
+package com.atguigu;
+
+/**
+ * @author snkstart
+ * @create 2022-07-08 23:43
+ */
+public class gitest {
+        public static void main(String[] args) {
+                System.out.println("git test");
+        }
+}
